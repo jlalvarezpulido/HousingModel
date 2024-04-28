e1f5380accef9da6215798d61a13bb0481e0b6c7
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2024-04-28 03:15:33 -0700

    terminal interface functional

--- housingmodel.py
+++ housingmodel.py
@@ -1,10 +1,14 @@
 import os
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
 
 # Read the dataset
 df = pd.read_csv('Housing.csv')
+df = df[["price", "bathrooms", "grade", "waterfront", "sqft_lot", "sqft_living", "lat", "long", "bedrooms"]]
 
 # Extracting features and target variable
 X = df[["bathrooms", "grade", "waterfront", "sqft_lot", "sqft_living", "lat", "long", "bedrooms"]]
@@ -12,8 +16,15 @@ y = df["price"]
 # Splitting data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=16, test_size =0.3)
 
-model = RandomForestRegressor(max_depth=6,random_state=0, n_estimators=10)
+model = RandomForestRegressor(max_depth=6, random_state=0, n_estimators=10)
 model.fit(X_train, y_train)
+
+# Predicting on the test set
+y_pred = model.predict(X_test)
+
+# Performance metrics
+mae_var = mean_absolute_error(y_test, y_pred)
+r_square = r2_score(y_test, y_pred)
 
 
 def cls() -> None:
@@ -22,28 +33,47 @@ def cls() -> None:
 
 def predict() -> None:
     userResponse = pd.DataFrame()
-    bathrooms = input("Enter Number of bathroms: ")
-    grade = input("Enter the grade of the home 1-13: ")
-    waterfront = input("Enter if the home is in a waterfront (0 for no or 1 for yes): ")
-    sqft_lot = input("Enter the square-footage of the lot (520-1650000): ")
-    sqft_living = input("Enter the square-footage of living area (290-13500): ")
-    lat = input("Enter the lattitude of the home: ")
-    long = input("Enter the longitude of the home: ")
-    bedrooms = input("Enter Number of bedrooms: ")
-    userResponse['bathrooms'] = [bathrooms]  # 0-8
-    userResponse['grade'] = [grade]#1-13 moves the price drastically
-    userResponse['waterfront'] = [waterfront]#1 or 0
-    userResponse['sqft_lot'] = [sqft_lot]#520 - 1650000
-    userResponse['sqft_living'] = [sqft_living]#290 - 13500
+    bathrooms = int(input("Enter Number of bathroms: "))
+    while bathrooms < 0:
+        bathrooms = int(input("Invalid Entry\nEnter Number of bathroms: "))
+    grade = int(input("Enter the grade of the home 1-13: (grade increases with more amenities)"))
+    while 0 >= grade or grade > 13:
+        grade = int(input("Invalid Entry\nEnter the grade of the home 1-13: (grade increases with more amenities) "))
+    waterfront = int(input("Enter if the home is in a waterfront (0 for no or 1 for yes): "))
+    while 0 > waterfront or waterfront > 1:
+        waterfront = int(input("Invalid Entry\nEnter if the home is in a waterfront (0 for no or 1 for yes): "))
+    sqft_lot = float(input("Enter the square-footage of the lot (520-1650000): "))
+    while sqft_lot < 0:
+        sqft_lot = float(input("Invalid Entry\nEnter the square-footage of the lot (520-1650000): "))
+    sqft_living = float(input("Enter the square-footage of living area (290-13500): "))
+    while sqft_living < 0:
+        sqft_living = float(input("Invalid Entry\nEnter the square-footage of living area (290-13500): "))
+    lat = float(input("Enter the lattitude of the home: "))
+    while 90 < lat or lat < -90:
+        lat = float(input("Invalid Entry\nEnter the lattitude of the home: "))
+    long = float(input("Enter the longitude of the home: "))
+    while 180 < long or long < -180:
+        long = float(input("Enter the longitude of the home: "))
+    bedrooms = int(input("Enter Number of bedrooms: "))
+    while bedrooms < 0:
+        bedrooms = int(input("Invalid Entry\nEnter Number of bedrooms: "))
+    userResponse['bathrooms'] = [bathrooms]
+    userResponse['grade'] = [grade]
+    userResponse['waterfront'] = [waterfront]
+    userResponse['sqft_lot'] = [sqft_lot]
+    userResponse['sqft_living'] = [sqft_living]
     userResponse['lat'] = [lat]
     userResponse['long'] = [long]
-    userResponse['bedrooms'] = [bedrooms]#1-33
+    userResponse['bedrooms'] = [bedrooms]
     # Predict the price
     predicted_price = model.predict(userResponse)
     print(f"Predicted price for this home specification is ${predicted_price[0]:.2f}")
+    log = input("would you like to log this prediction (enter 'y' for yes): ")
+    if log == 'y':
+        userResponse['predicted_price'] = predicted_price[0]
+        userResponse.to_csv('log.csv', mode='a', index=False, header=False)
     input("press enter to return")
     cls()
-
 
 
 def commands() -> None:
@@ -51,7 +81,11 @@ def commands() -> None:
     print("predict: Will begin a prediction query")
     print("city_list: Will print a list of popular cities in Washington with latitude and logitude values.")
     print("commands: display a list of valid commands")
-    print("commands: display a list of valid commands")
+    print("heatmap: displays descriptvie heatmap of the data")
+    print("distribution: displays descriptvie price distribution of the data")
+    print("scatterplot: displays descriptvie scatter plot of the price vs sqft_living")
+    print("r2: displays the R-Squared value")
+    print("mae: displays the mean absolute error")
     input("press enter to return")
 
 
@@ -69,10 +103,42 @@ def city_list() -> None:
     print("Federal Way: Latitude 47.32, Longitude -122.31\n")
 
 
-event_loop = True
+def heatmap() -> None:
+    sns.heatmap(df.corr(), annot=True)
+    plt.title("Correlation Heatmap")
+    plt.show()
+
+
+def scatterplot() -> None:
+    plt.figure(figsize=(10, 8))
+    sns.scatterplot(x='sqft_living', y='price', data=df)
+    plt.title('price vs sqft_living')
+    plt.xlabel('Living Area in Square feet')
+    plt.ylabel('Price')
+    plt.show()
+
+
+def distribution() -> None:
+    plt.figure(figsize=(10, 8))
+    sns.histplot(df['price'])
+    plt.title('Price Distribution')
+    plt.xlabel('Price')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+def r2() -> None:
+    print(f'\nThe R-Squared value is: {r_square}')
+
+
+def mae() -> None:
+    print(f'\nThe mean absolute error is: ${mae_var:.2f}')
+
+
+app_loop = True
 count = 0
 
-while event_loop:
+while app_loop:
     if count == 0:
         print("Welcome to the Housing Prediction Model Terminal application")
         count += 1
@@ -85,4 +151,14 @@ while event_loop:
         case "commands":
             commands()
         case "quit":
-            event_loop = False
+            app_loop = False
+        case "heatmap":
+            heatmap()
+        case "distribution":
+            distribution()
+        case "scatterplot":
+            scatterplot()
+        case "r2":
+            r2()
+        case "mae":
+            mae()
